327b2c5db259984c0bc4d41268dccd2eed06cfb5
Andrew <codesmith32@gmail.com>
2025-09-20 11:29:26 -0400

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig({
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      sourceType: "module",
+      ecmaVersion: "latest",
+    },
+  },
+  plugins: {
+    "@typescript-eslint": tseslint.plugin,
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        // allow _ prefixing for unused vars
+        args: "all",
+        argsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+        destructuredArrayIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+  },
+  files: ["./**/*.{ts,tsx}"],
+});
